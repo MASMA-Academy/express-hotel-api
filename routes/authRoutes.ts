d71c880215ca7d6464dd0723b34b1d2bf35b25ee
peji6ad <peji6ad@gmail.com>
2025-05-22 10:36:12 +0800

    add promote user to admin

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,10 +1,13 @@
 import { express } from "../deps.ts";
 import * as AuthController from "../controllers/authController.ts";
+import { adminOnly, protect } from "../middlewares/authMiddleware.ts";
+
 
 const router = express.Router();
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
+router.post("/promote", protect, adminOnly, AuthController.promote);
 
 export { router as authRouter };
  
